ddd09905c45280c2faa119e8247e686b133422fc
mikolajzalewski <mikolajzalewskigm@gmail.com>
2022-10-30 18:25:14 +0100

    Update app.py
    add screensaver

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 import cv2
+import requests
 import streamlit as st
+
+from streamlit_lottie import st_lottie
+from PIL import Image
+
+st.set_page_config(page_title="Age Prediction", page_icon=":anger:")
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
 # website title and description 
 st.title("Real-time age prediction")
@@ -10,8 +22,10 @@ run = st.checkbox('Run')
 FRAME_WINDOW = st.image([])
 camera = cv2.VideoCapture(0)
 
+lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
 
 while run:
+    st.empty()
     # capture image from the camera
     _, frame = camera.read()
 
@@ -25,4 +39,5 @@ while run:
     # show the final image
     FRAME_WINDOW.image(frame)
 else:
-    st.write('Stopped')
+    st_lottie(lottie_coding, height=400)
+    st.title('App stopped - click "Run" to start')
