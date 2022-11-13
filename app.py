4cc488f47016b31ca0fe58d9fd12232e5ca87f9f
Jakub Swistak <kontakt@jakub0301.net>
2022-11-13 12:34:21 +0100

    Added facial recognition to the app

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import cv2
 import requests
 import streamlit as st
-
+import mediapipe as mp
 from streamlit_lottie import st_lottie
 from PIL import Image
+
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
 
 st.set_page_config(page_title="Age Prediction", page_icon=":anger:")
 
@@ -24,22 +27,30 @@ camera = cv2.VideoCapture(0)
 
 # loading the animation
 lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
+with mp_face_detection.FaceDetection(
+    model_selection=0, min_detection_confidence=0.5) as face_detection:
+    while run:
+        st.empty()
+        # capture image from the camera
+        _, frame = camera.read()
 
-while run:
-    st.empty()
-    # capture image from the camera
-    _, frame = camera.read()
-
-    # convert BGR colours to RGB
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    
-    # <code>
-    # machine learning things
-    # </code>
-
-    # show the final image
-    FRAME_WINDOW.image(frame)
-else:
-    # if app is not working - display screensaver
-    st_lottie(lottie_coding, height=400)
-    st.markdown("<h1 style='text-align: center; color: grey;'>App is turned off</h1>", unsafe_allow_html=True)
+        # convert BGR colours to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = face_detection.process(image)
+        
+        image.flags.writeable = True
+        
+        if results.detections:
+            for detection in results.detections:
+                 # <code>
+                # machine learning things - model prediction for detection and printing age on the image
+                # </code>
+                mp_drawing.draw_detection(image, detection)
+            
+        
+        # show the final image
+        FRAME_WINDOW.image(image)
+    else:
+        # if app is not working - display screensaver
+        st_lottie(lottie_coding, height=400)
+        st.markdown("<h1 style='text-align: center; color: grey;'>App is turned off</h1>", unsafe_allow_html=True)
