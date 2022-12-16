14416b9218a3a1ec1abc775b71d1b2fce6d354b7
ashleve <lukasz.zalewski.ai@gmail.com>
2022-12-16 15:45:04 +0100

    Format files

--- src/app.py
+++ src/app.py
@@ -1,16 +1,16 @@
 import pyrootutils
+
 root = pyrootutils.setup_root(__file__, pythonpath=True, cwd=True)
 
 import cv2
 import mediapipe as mp
 import requests
 import streamlit as st
+from mediapipe.python.solutions.drawing_utils import _normalized_to_pixel_coordinates
 from PIL import Image
 from streamlit_lottie import st_lottie
-from mediapipe.python.solutions.drawing_utils import _normalized_to_pixel_coordinates
 
 from src.predict import Predict
-
 
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
@@ -54,56 +54,66 @@ with mp_face_detection.FaceDetection(
         image.flags.writeable = True
 
         if results.detections:
-            #print(results.detections)
+            # print(results.detections)
             for detection in results.detections:
-                
+
                 relative_bounding_box = detection.location_data.relative_bounding_box
                 rect_start_point = _normalized_to_pixel_coordinates(
-                    relative_bounding_box.xmin,
-                    relative_bounding_box.ymin,
-                    image_cols,
-                    image_rows
+                    relative_bounding_box.xmin, relative_bounding_box.ymin, image_cols, image_rows
                 )
                 rect_end_point = _normalized_to_pixel_coordinates(
                     relative_bounding_box.xmin + relative_bounding_box.width,
                     relative_bounding_box.ymin + relative_bounding_box.height,
                     image_cols,
-                    image_rows
+                    image_rows,
                 )
-                
+
                 if rect_start_point is not None and rect_end_point is not None:
                     width = rect_end_point[0] - rect_start_point[0]
                     height = rect_end_point[1] - rect_start_point[1]
-                    resized_width_params = (int(rect_start_point[0] - width * SIZE),
-                                             int(rect_end_point[0] + width * SIZE)
-                                            )
-                    
-                    resized_height_params = (int(rect_start_point[1] - height * SIZE),
-                                             int(rect_end_point[1] + height * SIZE)
-                                            )
+                    resized_width_params = (
+                        int(rect_start_point[0] - width * SIZE),
+                        int(rect_end_point[0] + width * SIZE),
+                    )
+
+                    resized_height_params = (
+                        int(rect_start_point[1] - height * SIZE),
+                        int(rect_end_point[1] + height * SIZE),
+                    )
 
                     image_height, image_width, _ = image.shape
-                    resized_width_params = (max(0, resized_width_params[0]), 
-                                            min(image_width, resized_width_params[1])
-                                            )
-                    resized_height_params = (max(0, resized_height_params[0]),
-                                            min(image_height, resized_height_params[1])
-                                            )
-                    
-                    cropped_image = image[resized_height_params[0]:resized_height_params[1],
-                                         resized_width_params[0]:resized_width_params[1]]
-                    
-                    
+                    resized_width_params = (
+                        max(0, resized_width_params[0]),
+                        min(image_width, resized_width_params[1]),
+                    )
+                    resized_height_params = (
+                        max(0, resized_height_params[0]),
+                        min(image_height, resized_height_params[1]),
+                    )
+
+                    cropped_image = image[
+                        resized_height_params[0] : resized_height_params[1],
+                        resized_width_params[0] : resized_width_params[1],
+                    ]
+
                     cv2.imwrite("image.jpg", cv2.cvtColor(cropped_image, cv2.COLOR_RGB2BGR))
-                    
+
                     face = Image.fromarray(cropped_image)
                     face = face.convert("RGB")
 
                     prediction = model.predict(face)
                     print(prediction)
-                    image = cv2.putText(image, f"Age: {int(prediction)}", (rect_start_point[0], rect_start_point[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-                    
-                
+                    image = cv2.putText(
+                        image,
+                        f"Age: {int(prediction)}",
+                        (rect_start_point[0], rect_start_point[1] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5,
+                        (255, 255, 255),
+                        2,
+                        cv2.LINE_AA,
+                    )
+
                 mp_drawing.draw_detection(image, detection)
 
         # show the final image
--- src/predict.py
+++ src/predict.py
@@ -1,12 +1,9 @@
-import pytorch_lightning as pl
-import torch
 from torchvision import transforms
-from src.data.face_age_dataset import FaceAgeDataset
-from PIL import Image
 
 from src.train import FaceAgeModule
 
-class Predict():
+
+class Predict:
     def __init__(self):
         self.model = FaceAgeModule.load_from_checkpoint("models/best-checkpoint.ckpt")
         self.model.eval()
@@ -15,9 +12,10 @@ class Predict():
             transforms.ToTensor(),
             transforms.Resize((100, 100)),
             transforms.Resize((224, 224)),
-            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
         ]
         self.transform = transforms.Compose(transform_list)
+
     def predict(self, image) -> float:
         img = self.transform(image)
         img = img.reshape(1, 3, 224, 224)
